872d74b78aa767cacc04cf0de870f8b1d28907fe
PrakashTiwari-dotcom <prakashtiwari901@gmail.com>
2020-06-15 14:22:41 +0545

    Updates

---
+++ precache-manifest.96e8c0683ded1f0df6a389138affc220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73562ac5da52af6f2cafafc89551d8ff",
+    "url": "/monosters-rolodex/index.html"
+  },
+  {
+    "revision": "7bef94f4341d7416f760",
+    "url": "/monosters-rolodex/static/css/main.e4692d04.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monosters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monosters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bef94f4341d7416f760",
+    "url": "/monosters-rolodex/static/js/main.c55dc435.chunk.js"
+  },
+  {
+    "revision": "b847bdfed64417a1134f",
+    "url": "/monosters-rolodex/static/js/runtime-main.465287d6.js"
+  }
+]);
